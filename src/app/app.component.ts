5f16045aa70b5f36f3bd381390e32f376ab46454
tengzhang <ztengc5@gmail.com>
2022-10-17 13:25:59 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-OAuth2';
+  GoogleAuth: any;
+  isAuthorized: boolean = false;
+  user: any;
+  username: string = 'username';
+
+  DISCOVERY_DOCS: Array<string> = [
+    "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+  ];
+  SCOPES: string = "https://www.googleapis.com/auth/drive.metadata.readonly";
+  API_KEY: string = "YOUR_API_KEY";
+  CLIENT_ID: string = "YOUR_CLIENT_ID";
+
+
+  constructor(private ngZone: NgZone) { }
+
+  ngOnInit(): void {
+    gapi.load("client:auth2", this.initClient);
+  }
+
+  initClient = () => {
+    gapi.client
+      .init({
+        apiKey: this.API_KEY,
+        discoveryDocs: this.DISCOVERY_DOCS,
+        clientId: this.CLIENT_ID,
+        scope: this.SCOPES,
+      })
+      .then(() => {
+        this.ngZone.run(() => {
+          this.GoogleAuth = gapi.auth2.getAuthInstance();
+          this.GoogleAuth.isSignedIn.listen(this.updateSigninStatus);
+          this.setSigninStatus();
+        })
+      });
+  }
+
+  setSigninStatus = () => {
+    this.ngZone.run(() => {
+      this.user = this.GoogleAuth.currentUser.get();
+      this.isAuthorized = this.user.hasGrantedScopes(this.SCOPES);
+      this.getUserName();
+    })
+  }
+
+  updateSigninStatus = () => {
+    this.ngZone.run(() => {
+      this.setSigninStatus();
+    });
+  }
+
+  getUserName = () => {
+    const profile = this.user.getBasicProfile();
+    this.username = profile?.zf;
+  }
+
+  handleAuthClick = () => {
+    if (this.GoogleAuth.isSignedIn.get()) {
+      this.GoogleAuth.signOut();
+    } else {
+      this.GoogleAuth.signIn();
+    }
+  }
 }
